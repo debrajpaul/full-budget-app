1f72c61e9b10fa6f165737a63d9be63a8706c99f
Debraj Paul <debrajpaul@Debrajs-MacBook-Air.local>
2025-07-21 16:38:47 +0530

    fix: remove prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,6 @@ import js from "@eslint/js";
 import pluginTs from "@typescript-eslint/eslint-plugin";
 import parserTs from "@typescript-eslint/parser";
 import pluginJest from "eslint-plugin-jest";
-import pluginPrettier from "eslint-plugin-prettier";
-import prettier from "eslint-config-prettier";
 import globals from "globals";
 
 /** @type {import("eslint").FlatConfig[]} */
@@ -22,12 +20,10 @@ export default [
     },
     plugins: {
       "@typescript-eslint": pluginTs,
-      prettier: pluginPrettier,
     },
     rules: {
       ...pluginTs.configs.recommended.rules,
       "@typescript-eslint/no-explicit-any": "warn",
-      "prettier/prettier": "error",
     },
   },
   {
@@ -44,5 +40,4 @@ export default [
       "jest/no-focused-tests": "error",
     },
   },
-  prettier,
 ];
