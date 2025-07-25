2e399acd057ba863550b9997d5c4554c99faa098
Debraj Paul <debrajpaul@Debrajs-MacBook-Air.local>
2025-07-25 16:30:55 +0530

    fix: save transaction normalized

--- packages/db/src/models/transaction.ts
+++ packages/db/src/models/transaction.ts
@@ -38,24 +38,30 @@ export class TransactionStore implements ITransactionStore {
       this.logger.info(`Saving transaction: ${txn.transactionId}`);
       this.logger.debug("Transaction", { txn });
       const PK = `USER#${txn.userId}`,
-        SK = `TXN#${txn.transactionId}`;
-      const command = new PutCommand({
-        TableName: this.tableName,
-        Item: {
+        SK = `TXN#${txn.transactionId}`,
+        item = {
           PK,
           SK,
+          userId: { S: txn.userId },
+          transactionId: { S: txn.transactionId },
           bankName: { S: txn.bankName },
           amount: { N: txn.amount.toString() },
           txnDate: { S: new Date(txn.txnDate).toISOString() },
           createdAt: { S: txn.createdAt },
           updatedAt: { S: txn.updatedAt },
           ...(txn.description && { description: { S: txn.description } }),
+          ...(txn.balance !== undefined && {
+            balance: { N: txn.balance.toString() },
+          }),
           ...(txn.category && { category: { S: txn.category } }),
           ...(txn.type && { type: { S: txn.type } }),
           ...(txn.deletedAt && {
             deletedAt: { S: new Date(txn.deletedAt).toISOString() },
           }),
-        },
+        };
+      const command = new PutCommand({
+        TableName: this.tableName,
+        Item: item,
         ConditionExpression:
           "attribute_not_exists(PK) AND attribute_not_exists(SK)",
       });
