75f166704a6b108b3c22fffa311344bd0381091e
Debraj Paul <debrajpaul@Debrajs-MacBook-Air.local>
2025-08-07 16:37:56 +0530

    feat: bug fix

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
-const path = require("path")
-const { pathsToModuleNameMapper } = require("ts-jest");
-const { compilerOptions } = require("./tsconfig.json"); 
+import path from "path";
+import { pathsToModuleNameMapper } from "ts-jest";
+import { fileURLToPath } from "url";
+import { readFileSync } from "fs";
+
+// __dirname equivalent in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Read and parse tsconfig
+const tsconfig = JSON.parse(
+  readFileSync(new URL("./tsconfig.json", import.meta.url), "utf-8")
+);
 
 export default {
   preset: "ts-jest",
@@ -8,10 +18,10 @@ export default {
   testMatch: ["**/?(*.)+(spec|test).ts"],
   moduleFileExtensions: ["ts", "js", "json"],
   collectCoverage: false,
-  coverageDirectory: path.resolve(__dirname,'./coverage'),
+  coverageDirectory: path.resolve(__dirname, "./coverage"),
   coverageReporters: ["text", "lcov"],
   reporters: ["default"],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: __dirname,
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+    prefix: "<rootDir>/",
   }),
 };
