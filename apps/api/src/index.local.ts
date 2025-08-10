7048b963f5e95d3da3aaea3ecf935a9792780797
Debraj Paul <debrajpaul@Debrajs-MacBook-Air.local>
2025-08-10 14:46:40 +0530

    feat: code refactor

---
+++ apps/api/src/index.local.ts
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+import { config } from "./environment";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@as-integrations/express5";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { schema } from "./schemas/schema";
+import { createContext } from "./context";
+import { IGraphQLContext } from "@common";
+
+dotenv.config();
+async function startServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer<IGraphQLContext>({
+    schema,
+    cache: "bounded",
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+
+  app.use(
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: createContext,
+    }),
+  );
+
+  httpServer.listen(config.port, () => {
+    console.log(
+      `🚀 Apollo Server ready at http://localhost:${config.port}/graphql`,
+    );
+  });
+}
+
+startServer();
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,40 +1,26 @@
-import dotenv from "dotenv";
-import { config } from "./environment";
 import { ApolloServer } from "@apollo/server";
-import { expressMiddleware } from "@as-integrations/express5";
-import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import express from "express";
-import cors from "cors";
-import http from "http";
+import {
+  startServerAndCreateLambdaHandler,
+  handlers,
+} from "@as-integrations/aws-lambda";
 import { schema } from "./schemas/schema";
-import { createContext } from "./context";
 import { IGraphQLContext } from "@common";
+import { createContext } from "./context";
 
-dotenv.config();
-async function startServer() {
-  const app = express();
-  const httpServer = http.createServer(app);
+const server = new ApolloServer<IGraphQLContext>({
+  schema,
+  cache: "bounded",
+});
 
-  const server = new ApolloServer<IGraphQLContext>({
-    schema,
-    cache: "bounded",
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
-  await server.start();
-
-  app.use(
-    cors(),
-    express.json(),
-    expressMiddleware(server, {
-      context: createContext,
-    }),
-  );
-
-  httpServer.listen(config.port, () => {
-    console.log(
-      `🚀 Apollo Server ready at http://localhost:${config.port}/graphql`,
-    );
-  });
-}
-
-startServer();
+export const handler = startServerAndCreateLambdaHandler(
+  server,
+  handlers.createAPIGatewayProxyEventV2RequestHandler(),
+  {
+    context: async ({ event, context }) => {
+      return createContext({
+        event,
+        context,
+      });
+    },
+  },
+);
