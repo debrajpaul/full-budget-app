165f433510184dfd148032ab110e264f9bde756e
Debraj Paul <debrajpaul@Debrajs-MacBook-Air.local>
2025-08-26 10:50:11 +0530

    feat: added test cases

--- packages/services/src/index.ts
+++ packages/services/src/index.ts
@@ -3,4 +3,3 @@ export * from "./upload-statement-service";
 export * from "./authorization-service";
 export * from "./transaction-service";
 export * from "./custom-error";
-export * from "./nlp-service";
