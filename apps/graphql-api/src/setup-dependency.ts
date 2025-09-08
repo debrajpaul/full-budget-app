c6703406114c51070f136797badca40529e51ca0
Debraj Paul <debrajpaul@Debrajs-MacBook-Air.local>
2025-09-08 13:56:54 +0530

    feat: bug fix

--- apps/graphql-api/src/setup-dependency.ts
+++ apps/graphql-api/src/setup-dependency.ts
@@ -3,12 +3,12 @@ import { WinstonLogger } from "@logger";
 import { S3 } from "@aws-sdk/client-s3";
 import { SQS } from "@aws-sdk/client-sqs";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { ComprehendClientConfig } from "@aws-sdk/client-comprehend";
+import { ComprehendClient } from "@aws-sdk/client-comprehend";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-import { ComprehendClientFactory } from "@client";
 
 export function setupDependency() {
   const client = new DynamoDBClient({ region: config.awsRegion });
+  const comprehendClient = new ComprehendClient({ region: config.awsRegion });
   const dynamoDBDocumentClient = DynamoDBDocumentClient.from(client);
   const logger = WinstonLogger.getInstance(config.logLevel);
   const s3Client = new S3({
@@ -17,15 +17,11 @@ export function setupDependency() {
   const sqsClient = new SQS({
     region: config.awsRegion,
   });
-  const comprehendClientFactory = new ComprehendClientFactory(
-    logger.child("ComprehendClient"),
-    { region: config.awsRegion } as ComprehendClientConfig,
-  );
   return {
     logger,
     s3Client,
     sqsClient,
     dynamoDBDocumentClient,
-    comprehendClientFactory,
+    comprehendClient,
   };
 }
